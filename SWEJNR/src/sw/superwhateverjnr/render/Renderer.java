d7cfd20e179dc6df7c811cd5c9e8ba0b194a4b9b
weaondara <wea_ondara@alpenblock.net>
2014-04-25 17:49:37 +0200

    removed commented code; calculated in prepare

--- SWEJNR/src/sw/superwhateverjnr/render/Renderer.java
+++ SWEJNR/src/sw/superwhateverjnr/render/Renderer.java
@@ -123,48 +123,6 @@ public class Renderer
 	}
 	private void drawWorld()
 	{
-//		Location min=game.getMinDisplayPoint();
-//		int x1=(int) Math.floor(min.getX());
-//		int y1=(int) Math.floor(min.getY());
-//		int x2=(int) (x1+Math.ceil((double)game.getDisplayWidth()/game.getTextureWidth()))+(min.getX()%1==0?0:1);
-//		int y2=(int) (y1+Math.ceil((double)game.getDisplayHeight()/game.getTextureHeight()))+(min.getY()%1==0?0:1);
-//		
-//		int xstart=x1<0?0:x1;
-//		int ystart=y1<0?0:y1;
-//		int xend=x2>world.getWidth()?world.getWidth():x2;
-//		int yend=y2>world.getHeight()?world.getHeight():y2;
-//		
-//		
-//		float leftoffset;
-//		if(min.getY()<0)
-//		{
-//			leftoffset=(float) ((min.getX()%1)*game.getTextureWidth());
-//		}
-//		else
-//		{
-//			leftoffset=(float) ((1-min.getX()%1)*game.getTextureWidth());
-//		}
-//		if(leftoffset>0)
-//		{
-//			leftoffset-=game.getTextureWidth();
-//		}
-//		
-//		
-//		float topoffset;
-//		if(min.getY()<0)
-//		{
-//			topoffset=(float) ((min.getY()%1)*game.getTextureHeight());
-//		}
-//		else
-//		{
-//			topoffset=(float) ((1-min.getY()%1)*game.getTextureHeight());
-//		}
-//		if(topoffset>0)
-//		{
-//			topoffset-=game.getTextureHeight();
-//		}
-		
-		
 		for(int x=xstart;x<xend;x++)
 		{
 			float left=leftoffset + (x-x1) * game.getTextureWidth();
