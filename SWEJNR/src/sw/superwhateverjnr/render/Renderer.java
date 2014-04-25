b3835e40c1abcfd02c92715506d929e0b6b1665e
weaondara <wea_ondara@alpenblock.net>
2014-04-25 14:12:52 +0200

    removed commented code and debuggin outputs

--- SWEJNR/src/sw/superwhateverjnr/render/Renderer.java
+++ SWEJNR/src/sw/superwhateverjnr/render/Renderer.java
@@ -43,11 +43,11 @@ public class Renderer
 		//world
 		drawWorld();
 		
-//		//entities
-//		drawEntities(c, p);
-//		
-//		//world
-//		drawPlayer(c, p);
+		//entities
+		drawEntities();
+		
+		//world
+		drawPlayer();
 		
 		return bitmap;
 	}
@@ -69,16 +69,6 @@ public class Renderer
 		int xend=x2>world.getWidth()?world.getWidth():x2;
 		int yend=y2>world.getHeight()?world.getHeight():y2;
 		
-//		if(x2>world.getWidth())
-//		{
-//			x2=world.getWidth();
-//		}
-//		if(y2>world.getHeight())
-//		{
-//			y2=world.getHeight();
-//		}
-//		int xmax=(int) (Math.ceil(x2)<world.getWidth()?Math.ceil(x2):world.getWidth());
-//		int ymax=(int) (Math.ceil(y2)<world.getHeight()?Math.ceil(y2):world.getHeight());
 		
 		float leftoffset;
 		if(min.getY()<0)
@@ -94,6 +84,7 @@ public class Renderer
 			leftoffset-=game.getTextureWidth();
 		}
 		
+		
 		float topoffset;
 		if(min.getY()<0)
 		{
@@ -108,30 +99,17 @@ public class Renderer
 			topoffset-=game.getTextureHeight();
 		}
 		
-//		System.out.println("leftoffset="+leftoffset+";topoffset="+topoffset);
-//		System.out.println("xmax="+xmax+";ymax="+ymax);
-//		System.out.println("x1="+x1+";y1="+y1);
-//		System.out.println("x2="+x2+";y2="+y2);
-//		System.out.println("game.getDisplayWidth()="+game.getDisplayWidth()+";game.getDisplayHeight()="+game.getDisplayHeight());
+		
 		for(int x=xstart;x<xend;x++)
 		{
 			float left=leftoffset + (x-x1) * game.getTextureWidth();
-//			System.out.println("left="+left);
 			for(int y=yend-1;y>ystart-1;y--)
 			{
-//				if( x>=world.getWidth() || //TODO too less performance
-//					y>=world.getHeight() || 
-//					x<0 || 
-//					y<0)
-//				{
-//					continue;
-//				}
 				Block b=world.getBlockAt(x, y);
 				if(b.getType()==Material.AIR)
 				{
 					continue;
 				}
-//				System.out.println("x="+x+";y="+y);
 				
 				IdAndSubId ref=new IdAndSubId(b.getType().getId(),b.getSubid());
 				Texture tex=TextureMap.getTexture(ref);
@@ -142,16 +120,15 @@ public class Renderer
 				}
 				
 				float top=topoffset+(y2-1-y)*game.getTextureHeight();
-//				System.out.println("left="+left+";top="+top);
 				canvas.drawBitmap(tex.getImage(), left, top, paint);
 			}
 		}
 	}
-	private void drawEntities(Canvas c, Paint p)
+	private void drawEntities()
 	{
 		
 	}
-	private void drawPlayer(Canvas c, Paint p)
+	private void drawPlayer()
 	{
 		
 	}
