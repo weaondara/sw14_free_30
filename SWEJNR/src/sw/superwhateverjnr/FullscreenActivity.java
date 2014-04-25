42a529ea0e689779ebb2f2ee6910e4e194aaa0b5
weaondara <wea_ondara@alpenblock.net>
2014-04-25 12:54:56 +0200

    file cleanup

--- SWEJNR/src/sw/superwhateverjnr/FullscreenActivity.java
+++ SWEJNR/src/sw/superwhateverjnr/FullscreenActivity.java
@@ -1,57 +1,18 @@
 package sw.superwhateverjnr;
 
-
 import lombok.Getter;
 
 import sw.superwhateverjnr.R;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
-public class FullscreenActivity extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
-    
+public class FullscreenActivity extends Activity 
+{
     @Getter
     private static FullscreenActivity instance;
 
@@ -69,8 +30,6 @@ public class FullscreenActivity extends Activity {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         }
         
-        setContentView(R.layout.activity_fullscreen);
-
         new Game();
     }
 }
