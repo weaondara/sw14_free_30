fca1425ed5d4d8abac19c53fb10c53f25b663a30
weaondara <wea_ondara@alpenblock.net>
2014-04-27 16:52:14 +0200

    added isonground function

--- SWEJNR/src/sw/superwhateverjnr/entity/Player.java
+++ SWEJNR/src/sw/superwhateverjnr/entity/Player.java
@@ -1,7 +1,13 @@
 package sw.superwhateverjnr.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import sw.superwhateverjnr.Game;
+import sw.superwhateverjnr.block.Block;
 import sw.superwhateverjnr.world.Location;
+import sw.superwhateverjnr.world.World;
 
+@Getter
 public class Player extends Entity
 {
 	private double moveArmswingDegrees;
@@ -13,6 +19,26 @@ public class Player extends Entity
 		standArmswingDegrees=0;
 	}
 	
+	@Setter
 	private boolean movingright;
+	@Setter
 	private boolean movingleft;
+	@Setter
+	private boolean jumping;
+	
+	public boolean isOnGround()
+	{
+		World w=Game.getInstance().getWorld();
+		if(w==null)
+		{
+			return false;
+		}
+		Block b=w.getBlockAt(location.add(0, -1));
+		if(!b.getType().isSolid())
+		{
+			return false;
+		}
+		
+		return location.getBlockY()==location.getY();
+	}
 }
