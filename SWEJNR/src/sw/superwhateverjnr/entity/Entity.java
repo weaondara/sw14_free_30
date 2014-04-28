78f4c6a66aa270c4abf979744bbc05880af9c749
weaondara <wea_ondara@alpenblock.net>
2014-04-28 22:33:45 +0200

    added properties for movement calculation

--- SWEJNR/src/sw/superwhateverjnr/entity/Entity.java
+++ SWEJNR/src/sw/superwhateverjnr/entity/Entity.java
@@ -1,10 +1,15 @@
 package sw.superwhateverjnr.entity;
 
+import com.google.common.base.Preconditions;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.Synchronized;
 import lombok.ToString;
+import sw.superwhateverjnr.Game;
 import sw.superwhateverjnr.util.Rectangle;
+import sw.superwhateverjnr.util.Vector;
 import sw.superwhateverjnr.world.Location;
 
 @Getter
@@ -24,8 +29,14 @@ public abstract class Entity
 	protected Location location;
 	protected Rectangle hitBox;
 	protected Rectangle renderBox;
+	@Setter
+	protected Vector velocity;
+	@Setter
 	protected double health;
+	@Setter
 	protected long lastJumpTime;
+	@Setter
+	protected long lastMoveTime;
 	
 	public Entity(EntityType type, Location location)
 	{
@@ -35,6 +46,7 @@ public abstract class Entity
 		hitBox=EntityInfoMap.getHitBox(type);
 		renderBox=EntityInfoMap.getRenderBox(type);
 		health=EntityInfoMap.getMaxHealth(type);
+		velocity=new Vector(0, 0);
 	}
 	public void jump()
 	{
@@ -43,8 +55,9 @@ public abstract class Entity
 	}
 	public void teleport(Location l)
 	{
+		Preconditions.checkElementIndex(l.getBlockX(), Game.getInstance().getWorld().getWidth());
+		Preconditions.checkElementIndex(l.getBlockY(), Game.getInstance().getWorld().getHeight());
 		location=l;
-		//TODO check if outside the world; if true then throw invalid arg exception
 		//TODO update rendering; not sure if we need it
 	}
 }
