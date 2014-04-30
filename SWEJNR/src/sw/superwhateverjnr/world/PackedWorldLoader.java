aa5bb21db7908789870e07b5cdd5823cf98a290a
weaondara <wea_ondara@alpenblock.net>
2014-04-30 09:42:10 +0200

    added instructions to packed worldloader

--- SWEJNR/src/sw/superwhateverjnr/world/PackedWorldLoader.java
+++ SWEJNR/src/sw/superwhateverjnr/world/PackedWorldLoader.java
@@ -6,6 +6,31 @@ public class PackedWorldLoader extends WorldLoader
 	@Override
 	public World loadWorld(String name)
 	{
+		//InputStream is=SWEJNR.getInstance().getResources().getAssets().open(file); //file is string; relative to project/assets
+		
+		//use sw.superwhateverjnr.io.FileReader for fiel reading
+		
+		
+		
+		/* World format
+		 * -------------------------------
+		 * string name
+		 * int width
+		 * int height
+		 * double spawnx
+		 * double spawny
+		 * 
+		 * loop
+		 *     int blockid
+		 *     byte subid
+		 *     int count extradata 
+		 *     loop
+		 *         int type extradata
+		 *         data extradata
+		 *     end loop
+		 * end loop
+		 * */
+		
 		return null;
 	}
 
