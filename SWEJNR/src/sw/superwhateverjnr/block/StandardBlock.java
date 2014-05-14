006fefefc42a80f7f2f22c062bdb64682866f40b
wea_ondara <wea_ondara@wea-ondara.net>
2014-05-14 10:51:17 +0200

    added second constructor

--- SWEJNR/src/sw/superwhateverjnr/block/StandardBlock.java
+++ SWEJNR/src/sw/superwhateverjnr/block/StandardBlock.java
@@ -1,5 +1,7 @@
 package sw.superwhateverjnr.block;
 
+import java.util.Map;
+
 import sw.superwhateverjnr.world.Location;
 
 public class StandardBlock extends Block
@@ -7,5 +9,9 @@ public class StandardBlock extends Block
 	protected StandardBlock(Location location, Material type)
 	{
 		super(location, type);
+	}
+	protected StandardBlock(Location location, Material type, byte subid, Map<String, Object> extraData)
+	{
+		super(location, type, subid, extraData);
 	}
 }
