2d9b47d658433e5dd48cb742a4b8fc37f4c98644
wea_ondara <wea_ondara@wea-ondara.net>
2014-06-09 05:27:37 +0200

    better values

--- SWEJNR/src/sw/superwhateverjnr/entity/EntityInfoMap.java
+++ SWEJNR/src/sw/superwhateverjnr/entity/EntityInfoMap.java
@@ -16,22 +16,25 @@ public class EntityInfoMap
 		renderBoxes=new HashMap<EntityType, Rectangle>();
 		healthMap=new HashMap<EntityType, Double>();
 		
-		hitBoxes.put(EntityType.PLAYER, new Rectangle(0, 0, 0.5, 1.75));
-		hitBoxes.put(EntityType.CREEPER, new Rectangle(0, 0, 0.5, 1.75));
-		hitBoxes.put(EntityType.SKELETON, new Rectangle(0, 0, 0.5, 1.75));
-		hitBoxes.put(EntityType.ZOMBIE, new Rectangle(0, 0, 0.5, 1.75));
+		hitBoxes.put(EntityType.PLAYER, new Rectangle(0, 0, 0.469, 1.875));
+		hitBoxes.put(EntityType.CREEPER, new Rectangle(0, 0, 0.75, 1.625));
+		hitBoxes.put(EntityType.SKELETON, new Rectangle(0, 0, 0.875, 2));
+		hitBoxes.put(EntityType.ZOMBIE, new Rectangle(0, 0, 0.891, 2));
+		hitBoxes.put(EntityType.SPIDER, new Rectangle(0, 0, 1.703, 0.781));
 		
 		
 		renderBoxes.put(EntityType.PLAYER, new Rectangle(0, 0, 1.5, 1.75));
 		renderBoxes.put(EntityType.CREEPER, new Rectangle(0, 0, 1.5, 1.75));
 		renderBoxes.put(EntityType.SKELETON, new Rectangle(0, 0, 1.5, 1.75));
 		renderBoxes.put(EntityType.ZOMBIE, new Rectangle(0, 0, 1.5, 1.75));
+		renderBoxes.put(EntityType.SPIDER, new Rectangle(0, 0, 1.5, 1.75));
 		
 		
 		healthMap.put(EntityType.PLAYER, 20D);
 		healthMap.put(EntityType.CREEPER, 20D);
 		healthMap.put(EntityType.SKELETON, 20D);
 		healthMap.put(EntityType.ZOMBIE, 20D);
+		healthMap.put(EntityType.SPIDER, 20D);
 	}
 	public static Rectangle getHitBox(EntityType type)
 	{
